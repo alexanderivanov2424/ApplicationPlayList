f3d8ec48eb61b11277300c6c6ae8852bb2519967
Jason Crowley <crowley.json@gmail.com>
2021-01-23 14:44:42 -0500

    Add functioning clear, save, load, load page buttons

--- popup.js
+++ popup.js
@@ -1,25 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // var checkPageButton = document.getElementById('save');
-    // checkPageButton.addEventListener('click', function() {
-    //         function changeColor(){
-    //             document.body.style.backgroundColor = "green";
-    //             window.scrollBy(0, 100);
-    //         }
-    //
-    //         chrome.tabs.executeScript({code: '(' + changeColor + ')();'}, function(result){
-    //             console.log(result);
-    //         })
-    //     });
+document.addEventListener('DOMContentLoaded', () => {
+  function get(keys) {
+    return new Promise(resolve => chrome.storage.local.get(keys, resolve));
+  }
 
-    var checkPageButton = document.getElementById('next');
-    checkPageButton.addEventListener('click', function() {
-            function changeColor(){
-                document.body.style.backgroundColor = "green";
-                window.scrollBy(0, 100);
-            }
+  function set(items) {
+    return new Promise(resolve => chrome.storage.local.set(items, resolve));
+  }
 
-            chrome.tabs.executeScript({code: '(' + changeColor + ')();'}, function(result){
-                console.log(result);
-            })
-        });
+  function clear() {
+    return new Promise(resolve => chrome.storage.local.clear(resolve));
+  }
+
+  function getCurrent() {
+    return new Promise(resolve => chrome.tabs.getCurrent(resolve));
+  }
+
+  function update(updateProperties) {
+    return new Promise(resolve => chrome.tabs.update(updateProperties, resolve));
+  }
+
+  async function save() {
+    const { data = [] } = await get(['data']);
+    const tab = await getCurrent();
+    const url = tab.url;
+    data.push(url);
+    await set({ data });
+    console.log('[+] Saved: ' + url);
+    return url;
+  }
+
+  async function load() {
+    const { data } = await get(['data']);
+    console.log('[+] Value currently is:', data);
+    return data;
+  }
+
+  async function loadPage() {
+    const data = await load();
+    await update({ url: data[0] });
+  }
+
+  document.getElementById('clear').addEventListener('click', clear);
+  document.getElementById('save').addEventListener('click', save);
+  document.getElementById('load').addEventListener('click', load);
+  document.getElementById('load-page').addEventListener('click', loadPage);
 });

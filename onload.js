70bda6b8c855a36337ad464552f4814f80aeb5cb
jwnicholas99 <jwnicholas99@gmail.com>
2021-01-23 16:10:16 -0500

    [FEAT] Add ability to move between saved apps
    * Rename functions to be specified as app-specific.
    * Save currIdx to chrome local memory.
    * Write funcs to move between next and prev apps.

---
+++ onload.js
@@ -0,0 +1,8 @@
+import {start} from "./popup.js";
+
+async function setUp(){
+    //await set({ currIdx: -1, apps: [] });
+}
+
+window.onload = setUp;
+
--- popup.js
+++ popup.js
@@ -1,47 +1,96 @@
+function get(keys) {
+  return new Promise(resolve => chrome.storage.local.get(keys, resolve));
+}
+
+function set(items) {
+  return new Promise(resolve => chrome.storage.local.set(items, resolve));
+}
+
+async function clear() {
+  //return new Promise(resolve => chrome.storage.local.clear(resolve));
+  await set({ currIdx: -1, apps: [] });
+}
+
+function query(queryInfo) {
+  return new Promise(resolve => chrome.tabs.query(queryInfo, resolve));
+}
+
+function update(updateProperties) {
+  return new Promise(resolve => chrome.tabs.update(updateProperties, resolve));
+}
+
+// Functions related to apps
+export async function getAllApps() {
+  const { apps } = await get(['apps']);
+  return apps;
+}
+
+export async function appendApp() {
+  const [tab] = await query({active: true, currentWindow: true});
+  const url = tab.url;
+
+  const { apps = [] } = await get(['apps']);
+  apps.push(url);
+  await set({ apps });
+
+  console.log('[+] Saved: ' + url);
+
+  const { currIdx } = await get(['currIdx']);
+  if (currIdx == -1) {
+    await set({currIdx: 0});
+  }
+  
+  return url;
+}
+
+export async function loadApp(idx) {
+  const apps = await getAllApps();
+  await update({ url: apps[idx] });
+}
+
+export async function loadCurrApp() {
+  const { currIdx } = await get(['currIdx']);
+  if (currIdx == -1) {
+    return;
+  }
+  await loadApp(currIdx);
+}
+
+export async function prevApp() {
+  let { currIdx } = await get(['currIdx']);
+  if (currIdx == -1) {
+    return;
+  }
+
+  currIdx = currIdx - 1;
+  currIdx = currIdx < 0 ? 0 : currIdx;
+  await set({ currIdx });
+  await loadApp(currIdx);
+}
+
+export async function nextApp() {
+  let { currIdx } = await get(['currIdx']);
+  if (currIdx == -1) {
+    return;
+  }
+
+  currIdx = currIdx + 1;
+  const currLength = (await getAllApps()).length;
+  currIdx = currIdx >= currLength ? currLength - 1 : currIdx;
+  await set({ currIdx });
+  await loadApp(currIdx);
+}
+
+export async function start() {
+  //await loadApp(0);
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  function get(keys) {
-    return new Promise(resolve => chrome.storage.local.get(keys, resolve));
-  }
-
-  function set(items) {
-    return new Promise(resolve => chrome.storage.local.set(items, resolve));
-  }
-
-  function clear() {
-    return new Promise(resolve => chrome.storage.local.clear(resolve));
-  }
-
-  function getCurrent() {
-    return new Promise(resolve => chrome.tabs.getCurrent(resolve));
-  }
-
-  function update(updateProperties) {
-    return new Promise(resolve => chrome.tabs.update(updateProperties, resolve));
-  }
-
-  async function save() {
-    const { data = [] } = await get(['data']);
-    const tab = await getCurrent();
-    const url = tab.url;
-    data.push(url);
-    await set({ data });
-    console.log('[+] Saved: ' + url);
-    return url;
-  }
-
-  async function load() {
-    const { data } = await get(['data']);
-    console.log('[+] Value currently is:', data);
-    return data;
-  }
-
-  async function loadPage() {
-    const data = await load();
-    await update({ url: data[0] });
-  }
-
   document.getElementById('clear').addEventListener('click', clear);
-  document.getElementById('save').addEventListener('click', save);
-  document.getElementById('load').addEventListener('click', load);
-  document.getElementById('load-page').addEventListener('click', loadPage);
+  document.getElementById('save').addEventListener('click', appendApp);
+  document.getElementById('load-app').addEventListener('click', loadCurrApp);
+  document.getElementById('previous').addEventListener('click', prevApp);
+  document.getElementById('next').addEventListener('click', nextApp);
 });

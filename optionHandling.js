fbcc5810da77cf2de288f60610ff95f7b8ab232c
Jason Crowley <crowley.json@gmail.com>
2021-01-24 12:24:32 -0500

    Save and load form data and radio buttons

--- optionHandling.js
+++ optionHandling.js
@@ -1,18 +1,23 @@
 import { set, get, clear } from "./utils.js";
 
-//"male", "female", "non-binary", "other", "no-say",
-const FIELDS = ["fname","lname", "gender", "email", "website", "linkedin",
-  "phone-num", "address", "zipcode", "country", "state", "birth-date",
-  "education", "major", "hear-about", "pronouns"];
+let optionsForm = null;
+
+////"male", "female", "non-binary", "other", "no-say",
+//const FIELDS = ["fname","lname", "gender", "email", "website", "linkedin",
+//  "phone-num", "address", "zipcode", "country", "state", "birth-date",
+//  "education", "major", "hear-about", "pronouns"];
+
+function formOptions() {
+  const options = {};
+  const formData = new FormData(optionsForm);
+  formData.forEach((value, key) => options[key] = value);
+  return options;
+}
 
 async function saveOptions() {
-  const options = {};
-  // const optionsForm = document.forms["options-form"]
-  for (const key of FIELDS) {
-    // element = document.getElementById(key)
-    options[key] = document.getElementById(key).value;
-  }
+  const options = formOptions();
   await set({ options });
+  await saveRadio('gender');
 }
 
 export async function getOptions() {
@@ -22,26 +27,38 @@ export async function getOptions() {
 
 async function loadOptions() {
   const { options } = await get("options");
-  const optionsForm = document.forms["options-form"]
 
-  for (const key of FIELDS) {
-    if(options[key]){
-      console.log(key);
-      const input = document.getElementById(key);
-      if(input.type === "radio"){
-        input.checked = True;
-        // input.value = options[key];
-      } else {
-        input.value = options[key];
-      }
-      // if(options[key]){
-      //   document.getElementById(key).value = options[key];
-      // }
+  for (const [key, value] of Object.entries(options)) {
+    const input = document.getElementById(key);
+    if (input) {
+      input.value = value;
     }
+  }
+  await loadRadio('gender');
+}
+
+async function saveRadio(key) {
+  let value = null;
+  const radios = document.querySelectorAll(`input[name=${key}]`);
+  for (const radio of radios) {
+    if (radio.checked) {
+      value = radio.value;
+    }
+  }
+  await set({ [key]: value });
+}
+
+async function loadRadio(key) {
+  const { [key]: value = null } = await get(key);
+  const radios = document.querySelectorAll(`input[name=${key}]`);
+  for (const radio of radios) {
+    radio.checked = radio.value === value;
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  optionsForm = document.forms['options-form'];
+  console.log(optionsForm);
   document.getElementById('save-options').addEventListener('click', saveOptions);
   document.getElementById('clear').addEventListener('click', clear);
 });

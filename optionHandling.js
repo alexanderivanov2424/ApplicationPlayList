52e140e0b28bad50108fafa8efa2905617e8f17f
Jason Crowley <crowley.json@gmail.com>
2021-01-24 15:14:49 -0500

    Simply optionHandling.js and restyle options.html

--- optionHandling.js
+++ optionHandling.js
@@ -1,66 +1,41 @@
-import { set, get, clear } from "./utils.js";
+import { set, get, clear } from './utils.js';
 
 let optionsForm = null;
 
-////"male", "female", "non-binary", "other", "no-say",
-//const FIELDS = ["fname","lname", "gender", "email", "website", "linkedin",
-//  "phone-num", "address", "zipcode", "country", "state", "birth-date",
-//  "education", "major", "hear-about", "pronouns"];
-
 function formOptions() {
-  const options = {};
   const formData = new FormData(optionsForm);
-  formData.forEach((value, key) => options[key] = value);
-  return options;
+  return Object.fromEntries(formData);
 }
 
 async function saveOptions() {
-  const options = formOptions();
-  await set({ options });
-  await saveRadio('gender');
-  await saveRadio('race');
+  await set({ options: formOptions() });
 }
 
 export async function getOptions() {
-  const { options } = await get("options");
+  const { options = formOptions() } = await get('options');
   return options;
 }
 
+function getInput(key, value) {
+  return document.querySelector(`[name=${key}][value=${value || 'none'}]`) || document.querySelector(`[name=${key}]`);
+}
+
 async function loadOptions() {
-  const { options } = await get("options");
+  const { options = formOptions() } = await get('options');
 
   for (const [key, value] of Object.entries(options)) {
-    const input = document.getElementById(key);
-    if (input) {
+    const input = getInput(key, value);
+    if (input.type === 'radio') {
+      input.checked = true;
+    } else {
       input.value = value;
     }
-  }
-  await loadRadio('gender');
-  await loadRadio('race');
-}
-
-async function saveRadio(key) {
-  let value = null;
-  const radios = document.querySelectorAll(`input[name=${key}]`);
-  for (const radio of radios) {
-    if (radio.checked) {
-      value = radio.value;
-    }
-  }
-  await set({ [key]: value });
-}
-
-async function loadRadio(key) {
-  const { [key]: value = null } = await get(key);
-  const radios = document.querySelectorAll(`input[name=${key}]`);
-  for (const radio of radios) {
-    radio.checked = radio.value === value;
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   optionsForm = document.forms['options-form'];
-  console.log(optionsForm);
+  optionsForm.addEventListener('submit', e => e.preventDefault());
   document.getElementById('save-options').addEventListener('click', saveOptions);
   document.getElementById('clear').addEventListener('click', clear);
 });

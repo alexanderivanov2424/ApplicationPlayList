4377ff8d75a5447078ceca554f6a923f67f41cf9
jwnicholas99 <jwnicholas99@gmail.com>
2021-01-24 00:47:03 -0500

    [FEAT] Styled popup.js and added appList rendering

--- popup.js
+++ popup.js
@@ -70,22 +70,47 @@ export async function start() {
   //await loadApp(0);
 }
 
-chrome.storage.onChanged.addListener(changes => {
+chrome.storage.onChanged.addListener(async changes => {
+  const { apps = [], currIdx } = await get(['apps', 'currIdx']);
+
   if ('currIdx' in changes) {
-    document.getElementById('index').innerHTML = changes['currIdx'].newValue;
+    renderAppList(apps, changes['currIdx'].newValue);
+  }
+
+  if ('apps' in changes){
+    renderAppList(changes['apps'].newValue, currIdx);
   }
 });
+
+function renderAppList(appUrls, currIdx) {
+	const appList = document.getElementById('app-list');
+	while (appList.firstChild) {
+	  appList.removeChild(appList.lastChild);
+	}
+
+	for (let i=0; i < appUrls.length; i++) {
+	  const appItem = document.createElement('li');
+	  appItem.innerHTML = appUrls[i];
+    if (i == currIdx) {
+      appItem.classList.add("highlight");
+    }
+	  appList.appendChild(appItem);
+	}
+}
 
 async function sendAutofillMessage() {
   const [tab] = await query({ active: true, currentWindow: true });
   chrome.tabs.sendMessage(tab.id, 'autofill');
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('clear').addEventListener('click', clear);
+document.addEventListener('DOMContentLoaded', async () => {
+  //document.getElementById('clear').addEventListener('click', clear);
   document.getElementById('save').addEventListener('click', appendApp);
   document.getElementById('start').addEventListener('click', loadCurrApp);
   document.getElementById('previous').addEventListener('click', prevApp);
   document.getElementById('next').addEventListener('click', nextApp);
   document.getElementById('autofill').addEventListener('click', sendAutofillMessage);
+  
+  const { apps = [], currIdx } = await get(['apps', 'currIdx']);
+  renderAppList(apps, currIdx);
 });

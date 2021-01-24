f6910627b60b3e2467e18241ebf549a45530787d
alexanderivanov2424 <alexander_ivanov@brown.edu>
2021-01-24 11:37:02 -0500

    mornginri

--- optionHandling.js
+++ optionHandling.js
@@ -1,12 +1,15 @@
 import { set, get, clear } from "./utils.js";
 
-const FIELDS = ["fname","lname", "email", "website", "linkedin",
+//"male", "female", "non-binary", "other", "no-say",
+const FIELDS = ["fname","lname", "gender", "email", "website", "linkedin",
   "phone-num", "address", "zipcode", "country", "state", "birth-date",
   "education", "major", "hear-about", "pronouns"];
 
 async function saveOptions() {
   const options = {};
+  // const optionsForm = document.forms["options-form"]
   for (const key of FIELDS) {
+    // element = document.getElementById(key)
     options[key] = document.getElementById(key).value;
   }
   await set({ options });
@@ -19,9 +22,21 @@ export async function getOptions() {
 
 async function loadOptions() {
   const { options } = await get("options");
+  const optionsForm = document.forms["options-form"]
+
   for (const key of FIELDS) {
     if(options[key]){
-      document.getElementById(key).value = options[key];
+      console.log(key);
+      const input = document.getElementById(key);
+      if(input.type === "radio"){
+        input.checked = True;
+        // input.value = options[key];
+      } else {
+        input.value = options[key];
+      }
+      // if(options[key]){
+      //   document.getElementById(key).value = options[key];
+      // }
     }
   }
 }
